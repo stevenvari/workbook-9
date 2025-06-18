2baf969aaa78629d2882229371bd1545a5c6ed3a
Steven Vargas <stevenvari13@gmail.com>
2025-06-18 15:14:14 -0400

    add my model class

---
+++ northwindtraders.api/src/main/java/com/pluralsight/northwindtraders/api/model/Category.java
@@ -0,0 +1,20 @@
+package com.pluralsight.northwindtraders.api.model;
+
+public class Category {
+
+    private int categoryId;
+    private String categoryName;
+
+    public Category(int categoryId, String categoryName) {
+        this.categoryId = categoryId;
+        this.categoryName = categoryName;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+}
---
+++ northwindtraders.api/src/main/java/com/pluralsight/northwindtraders/api/model/Product.java
@@ -0,0 +1,32 @@
+package com.pluralsight.northwindtraders.api.model;
+
+public class Product {
+
+    private int productId;
+    private String productName;
+    private int categoryId;
+    private double unitPrice;
+
+    public Product(int productId, String productName, int categoryId, double unitPrice) {
+        this.productId = productId;
+        this.productName = productName;
+        this.categoryId = categoryId;
+        this.unitPrice = unitPrice;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+}
